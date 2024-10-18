6103e2792789e39eed7990878cfafe1352f1a746
Kilian <k.leybros.15@gmail.com>
2024-10-18 08:04:19 +0200

    test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,21 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {JokesService} from "./services/jokes.service";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { JokesService } from './services/jokes.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
-      providers: [JokesService]
+      imports: [
+        HttpClientTestingModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+      ],
+      providers: [JokesService],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    const spy = spyOn(component,'getRandomJoke');
+    const spy = spyOn(app, 'getRandomJoke');
     app.ngOnInit();
     expect(spy).toHaveBeenCalled();
   });
