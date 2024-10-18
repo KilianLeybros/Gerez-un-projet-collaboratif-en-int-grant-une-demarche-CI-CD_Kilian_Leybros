17cdeaa75413b7a592059ae9ea0fd5dd521b5662
Kilian <k.leybros.15@gmail.com>
2024-10-18 08:20:07 +0200

    test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,24 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { JokesService } from './services/jokes.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [
-        HttpClientTestingModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatButtonModule,
-        MatCardModule,
-        MatDividerModule,
-      ],
+      imports: [HttpClientTestingModule],
       providers: [JokesService],
     }).compileComponents();
   });
@@ -27,8 +15,6 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    const spy = spyOn(app, 'getRandomJoke');
-    app.ngOnInit();
-    expect(spy).toHaveBeenCalled();
+    expect(app).toBeTruthy();
   });
 });
